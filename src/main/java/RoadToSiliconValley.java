7ee3759d6bf520401ccd1f502065296023bb6236
bastille1001 <timur.physics@mail.ru>
2020-05-13 21:36:35 +0400

    greeting

--- src/main/java/RoadToSiliconValley.java
+++ src/main/java/RoadToSiliconValley.java
@@ -1,5 +1,5 @@
 public class RoadToSiliconValley {
     public static void main(String[] args) {
-        System.out.println("I`m Greeting you");
+        System.out.println("I`m Greeting you!");
     }
 }
