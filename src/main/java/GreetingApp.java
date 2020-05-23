7195120d984483ae470560978624890054919412
mirzayev.mirzayev <mirzayev.mirzayev@gmail.com>
2020-05-24 01:00:54 +0400

    #Initial commit

---
+++ src/main/java/GreetingApp.java
@@ -0,0 +1,9 @@
+public class GreetingApp {
+    public static void main(String[] args) {
+        int x = 100;
+        while (x != 0){
+            System.out.println("Hello Calil");
+            x--;
+        }
+    }
+}
