836e403d1295cd3c2432538639bf83190039b95c
bastille1001 <timur.physics@mail.ru>
2020-05-23 23:05:18 +0400

    hi

---
+++ src/main/java/HelloJalil.java
@@ -0,0 +1,5 @@
+public class HelloJalil {
+    public static void main(String[] args) {
+        System.out.println("Road to Silicon Valley");
+    }
+}
--- src/main/java/RoadToSiliconValley.java
+++
@@ -1,5 +0,0 @@
-public class RoadToSiliconValley {
-    public static void main(String[] args) {
-        System.out.println("I`m Greeting you!!!!");
-    }
-}
