a0530efe8c094157c98142095e1944babc9f9abd
mirzayev.mirzayev <mirzayev.mirzayev@gmail.com>
2020-05-27 01:44:36 +0400

    Final

--- src/main/java/TinderApp.java
+++ src/main/java/TinderApp.java
@@ -13,9 +13,9 @@ import java.util.EnumSet;
 public class TinderApp {
     public static void main(String[] args) throws Exception {
 
-        final String url = "jdbc:postgresql://ec2-52-202-22-140.compute-1.amazonaws.com:5432/d5j53goiqufuts";
-        final String username = "wtgcnsjmuhjwcu";
-        final String pass = "63246260ebb4709c5fa444c4d6b356ce4d2dd0d964399a9dee00a1a3643e497d";
+//        final String url = "jdbc:postgresql://ec2-52-202-22-140.compute-1.amazonaws.com:5432/d5j53goiqufuts";
+//        final String username = "wtgcnsjmuhjwcu";
+//        final String pass = "63246260ebb4709c5fa444c4d6b356ce4d2dd0d964399a9dee00a1a3643e497d";
 
         Server server = new Server(Heroku.port());
         ServletContextHandler handler = new ServletContextHandler();
