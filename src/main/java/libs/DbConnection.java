cfcc7bd02eaea4c65cfb6b2290606c1dbdf8d9b6
Celilkhan <celil.ahmad@gmail.com>
2020-06-06 12:26:00 +0400

    #db

--- src/main/java/libs/DbConnection.java
+++ src/main/java/libs/DbConnection.java
@@ -15,10 +15,9 @@ public class DbConnection {
 
     private static final Logger LOG = LogManager.getFormatterLogger(LikesDAO.class);
 
-    private static final String url = "jdbc:postgresql://ec2-34-194-198-176.compute-1.amazonaws.com:5432/dcvoocgd20ti7v";
-
-    private static final String username = "hifexmldvumeij";
-    private static final String pass = "f88ff3edb228909aa2e02cc080e2012b918f781c87b9cb9eb2bd2acbf24ce1ed";
+    private static final String url = "JDBC_URL";
+    private static final String username = "JDBC_USER";
+    private static final String pass = "JDBC_PASS";
 
 
     private static Connection connection;
